c52d9f2a3312036b89f31e685ee1b2034479b5eb
Annie-2020 <41429556+20annie20@users.noreply.github.com>
2019-11-20 19:32:37 +0100

    update automat.c

--- automat.c
+++ automat.c
@@ -4,23 +4,27 @@
 
 #define MAX 1000
 
-int main(){
+int main()
+{
 	char instr[MAX];
 	int j = 0;
         int counter = 0;
         int c;
 
         printf("wprowadz instrukcje: \n");
-        while((c = getc(stdin)) != '\n'){
-        	if(c != ' '){ 
-				instr[counter] = c;
-				counter++;
-			}
+        while((c = getc(stdin)) != '\n')
+	{
+        	if(c != ' ')
+		{ 
+			instr[counter] = c;
+			counter++;
+		}
        	}
         
        	instr[counter] = '\0';
        	printf("\nInstrukcja to - ");
-       	while(instr[j] != '\0'){
+       	while(instr[j] != '\0')
+	{
                putchar(instr[j]);
                j++;
         }
@@ -34,64 +38,67 @@ int main(){
 	int i = 0;
 	int true = 0;
 	
-	for(i; i < counter; i++){ 
-	
+	for(i; i < counter; i++)
+	{ 
 		if(instr[i] != '1')
- 	   {
- 	   		if(instr[i] != '0'){
+ 	   	{
+ 	   		if(instr[i] != '0')
+			{
  	   			fprintf(stderr,  "niewlasciwy format danych wejsciowych: %c\n", instr[i]);
  	   			true = 1;
- 	   		   }
+ 	   		}
 		}	
    	}
    
    	if(true == 1)
 		    return 1;
 	
-	for(i = 0; i < counter; i++){ 
-		
-		switch(state){
+	for(i = 0; i < counter; i++)
+	{ 
+		switch(state)
+		{
 			case 0:
-				if(instr[i] == '1'){
+				if(instr[i] == '1')
+				{
 					state = 1;
 					str = "->q1";
 				}else{
-         	state = 2;
-          str = "->q2";
-	      }
+         				state = 2;
+          				str = "->q2";
+	      			}
 				break;
 			case 1:
 				if(instr[i] == '1'){
-           state = 0;
-           str = "->q0";
-        }else{
-           state = 3;
-           str = "->q3";
-        }
+					state = 0;
+					str = "->q0";
+       	 			}else{
+				  	state = 3;
+				  	str = "->q3";
+				}
 				break;
 			case 2:
 				if(instr[i] == '1'){
-           state = 3;
-           str = "->q3";
-        }else{
-           state = 0;
-           str = "->q0";
-        }
+				   	state = 3;
+				   	str = "->q3";
+        			}else{
+				   	state = 0;
+				   	str = "->q0";
+       				}
 				break;
 			case 3:
-			   if(instr[i] == '0'){
-            state = 1;
-            str = "->q1";
-         }
-         else if(instr[i] == '1'){
-            state = 2;
-            str = "->q2";
-         }
-			   break;
-  	   }
+			  	 if(instr[i] == '0'){
+				    	state = 1;
+				   	str = "->q1";
+			   	}
+         			else if(instr[i] == '1'){
+            			state = 2;
+            			str = "->q2";
+         			}
+			   	break;
+  	   	}
 
 		strcat(diagram, str);
-        printf("%s\n", diagram);	
+        	printf("%s\n", diagram);	
 	}
 	
 	if( state == 2)
